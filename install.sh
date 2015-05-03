91a46fb29a0f2fedccb63bb7d7df9687971746bf
Alex Soban <me@soban.co>
2015-05-03 14:19:52 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
 #!/usr/bin/env bash
+
+# Add repository key
+curl -sL http://nginx.org/keys/nginx_signing.key | apt-key add -
+
+# Add repository
+echo "deb http://nginx.org/packages/debian/ wheezy nginx" | tee /etc/apt/sources.list.d/nginx-org.list
+
+# Update repositories
+apt-get update
+
+# Install
+apt-get install -y nginx
+
+# Restart service
+service nginx restart
